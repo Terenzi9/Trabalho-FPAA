e06c133b120a9c45604aa1593c3f22801a3a5491
Terenzi9 <65312437+Terenzi9@users.noreply.github.com>
2023-06-18 15:57:32 -0300

    Adicionando arquivos de execução do trabalho

---
+++ Div_Conq_Freq.py
@@ -0,0 +1,56 @@
+import time
+from collections import Counter
+
+def merge_sort(palavras):
+    if len(palavras) <= 1:
+        return palavras
+
+    mid = len(palavras) // 2
+    left = merge_sort(palavras[:mid])
+    right = merge_sort(palavras[mid:])
+    
+    merged = merge(left, right)
+    return merged
+
+def merge(left, right):
+    merged = []
+    i = j = 0
+    
+    while i < len(left) and j < len(right):
+        if left[i][1] < right[j][1]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+    
+    while i < len(left):
+        merged.append(left[i])
+        i += 1
+    
+    while j < len(right):
+        merged.append(right[j])
+        j += 1
+    
+    return merged
+
+with open('palavras_300k.txt', 'r') as file:
+    conteudo = file.read()
+    palavras = conteudo.split()
+
+contador = Counter(palavras)
+start_time = time.time()
+frequencia_palavras = merge_sort(list(contador.items()))
+
+with open('final_Freq.txt', 'w') as output_file:
+    for word, count in frequencia_palavras:
+        for _ in range(count):
+            print(word, file=output_file)
+
+end_time = time.time()
+execution_time = end_time - start_time
+
+minutes = int(execution_time // 60)
+seconds = int(execution_time % 60)
+print(f"Execution time: {execution_time:.2f} seconds")
+
---
+++ Div_Conq_Ord.py
@@ -0,0 +1,59 @@
+import time
+
+def merge_sort(palavras):
+    if len(palavras) <= 1:
+        return palavras
+    
+    mid = len(palavras) // 2
+    left = merge_sort(palavras[:mid])
+    right = merge_sort(palavras[mid:])
+    
+    merged = merge(left, right)
+    return merged
+
+def merge(left, right):
+    merged = []
+    i = j = 0
+    
+    while i < len(left) and j < len(right):
+        if left[i].lower() < right[j].lower():
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+    
+    while i < len(left):
+        merged.append(left[i])
+        i += 1
+    
+    while j < len(right):
+        merged.append(right[j])
+        j += 1
+    
+    return merged
+
+# Read the palavras from palavras_100k.txt
+with open('palavras_300k.txt', 'r') as f:
+    conteudo = f.read()
+    palavras = conteudo.split()
+
+start_time = time.time()
+
+palavras_total = merge_sort(palavras)
+
+
+# Convert execution time to minutes and seconds
+
+
+# Write the sorted palavras to final.txt
+with open('final_Ord.txt', 'w') as output_file:
+    for palavra in palavras_total:
+        print(palavra, file=output_file)
+
+end_time = time.time()
+execution_time = end_time - start_time
+
+minutes = int(execution_time // 60)
+seconds = int(execution_time % 60)
+print(f"Execution time: {execution_time:.2f} seconds")
